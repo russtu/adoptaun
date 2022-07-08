9f4b343401fa81387900c84fe12ac0185dab1ad9
Ruben <rubcalop@gmail.com>
2022-07-08 12:08:14 +0200

    added folder structure + server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const cors = require('cors')
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+//Constantes
+const { PORT, BASE_URL } = process.env
+
+//Routes
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${BASE_URL} : ${PORT}`)
+})
