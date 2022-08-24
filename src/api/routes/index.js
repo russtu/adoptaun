2d72ec9af921d9c56e169a7e1ccdca927e871586
Ruben <rubcalop@gmail.com>
2022-08-24 18:37:34 +0200

    changed to a new architecture

---
+++ src/api/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const router = require('express').Router()
+
+
+
+//ANIMALS
+
+//ANIMALS SHELTERS
+
+//PHOTOS ANIMALS
+
+//PHOTOS SHELTERS
+
+module.exports = router
